2a52de7ce91784a1c897747e4228a1bec61b8786
András Gelencsér <andras.gelencser@gelencser.at>
2019-11-03 17:43:55 +0100

    Adaptation

--- regularisation_cv.R
+++ regularisation_cv.R
@@ -8,7 +8,6 @@ rmses_for_movie_lambdas <- function(train, test){
   movie_sum <- train %>% group_by(movieId) %>% summarise(s=sum(rating-avg), n_i=n())
   
   
-  cat("Calculate rmse with movie lambdas\n")
   for (l in lambdas){
     predicted_ratings <- test %>%
       left_join(movie_sum, by='movieId') %>%
@@ -17,11 +16,8 @@ rmses_for_movie_lambdas <- function(train, test){
       pull(pred)
     
     rmse_akt =RMSE(predicted_ratings, test$rating)
-    cat( "Lambda: ", str_pad(l,width = 4,side = "right"), " RMSE: ", rmse_akt, "\n")
     RMSE_all <- bind_rows(RMSE_all, data.frame(lambda = l, rmse = rmse_akt))
   }
-  cat("\n RMSE calculated\n")
-  cat("\n")
   return(RMSE_all)
 }
 
@@ -39,7 +35,6 @@ rmses_for_user_lambdas <- function(train, test, movie_lambda){
     group_by(userId) %>%
     summarize(s = sum(rating - b_movie - avg), n_i= n())
   
-  cat("Calculate rmse with user lambdas\n")
   for (l in lambdas){
     predicted_ratings <- test %>%
       left_join(b_movie, by='movieId') %>%
@@ -49,11 +44,8 @@ rmses_for_user_lambdas <- function(train, test, movie_lambda){
       pull(pred)
     
     rmse_akt =RMSE(predicted_ratings, test$rating)
-    cat( "Lambda: ", str_pad(l,width = 4,side = "right"), " RMSE: ", rmse_akt, "\n")
     RMSE_all <- bind_rows(RMSE_all, data.frame(lambda = l, rmse = rmse_akt))
   }
-  cat("\n RMSE calculated\n")
-  cat("\n")
   return(RMSE_all)
 }
 
@@ -68,7 +60,6 @@ cross_validation_movie <- function(trainset, lambdas, cv_n){
   for (i in c(1:cv_n)){
     
     part_count = data_count / cv_n
-    cat( "Run: ", i, " Test Part: ", str_pad((trunc((i-1) * part_count) + 1),width = 8,side = "left"), " ",  trunc(i * part_count), "\n")
     idx = c(   (trunc((i-1) * part_count) + 1) : trunc(i * part_count) )
     tmp = trainset_randomised[idx,]
     train = trainset_randomised[-idx,]
@@ -79,7 +70,6 @@ cross_validation_movie <- function(trainset, lambdas, cv_n){
     train <- rbind(train, removed)
     RMSE_Lambdas <- rmses_for_movie_lambdas(train, test)
     RMSE_all <- bind_rows(RMSE_all,RMSE_Lambdas %>% mutate(cv = i))
-    cat("\n")
   }
   return(RMSE_all)
 }
@@ -94,7 +84,6 @@ cross_validation_user <- function(trainset, lambdas, cv_n, movie_lambda){
   for (i in c(1:cv_n)){
     
     part_count = data_count / cv_n
-    cat( "Run: ", i, " Test Part: ", str_pad((trunc((i-1) * part_count) + 1),width = 8,side = "left"), " ",  trunc(i * part_count), "\n")
     idx = c(   (trunc((i-1) * part_count) + 1) : trunc(i * part_count) )
     tmp = trainset_randomised[idx,]
     train = trainset_randomised[-idx,]
@@ -105,20 +94,19 @@ cross_validation_user <- function(trainset, lambdas, cv_n, movie_lambda){
     train <- rbind(train, removed)
     RMSE_Lambdas <- rmses_for_user_lambdas(train, test, movie_lambda)
     RMSE_all <- bind_rows(RMSE_all,RMSE_Lambdas %>% mutate(cv = i))
-    cat("\n")
   }
   return(RMSE_all)
 }
 
 res <- cross_validation_movie(edx, lambdas,5)
 res <- res %>% group_by(lambda) %>% summarise(avg_rmse=mean(rmse))
-qplot(main=c('Lambda for movies'), lambdas, res$avg_rmse)
+qplot(main=c('Lambda for movies', 'RMSE', 'Lambda'), lambdas, res$avg_rmse)
 movie_lambda = lambdas[which.min(res$avg_rmse)]
 cat("Lambda for movie regulaisration: ", movie_lambda)
 
 res <- cross_validation_user(edx, lambdas,5, movie_lambda)
 res <- res %>% group_by(lambda) %>% summarise(avg_rmse=mean(rmse))
-qplot(main=c('Lambda for user'), lambdas, res$avg_rmse)
+qplot(main=c('Lambda for user', 'RMSE', 'Lambda'), lambdas, res$avg_rmse)
 user_lambda = lambdas[which.min(res$avg_rmse)]
 cat("Lambda for user regulaisration: ", user_lambda)
 
--- user_analysis.R
+++ user_analysis.R
@@ -1,12 +1,16 @@
 #calculate rating count pro user
-rating_count_pro_user <- edx %>% group_by(userId) %>% summarise(n = n(), avgRating=mean(rating), sdRating=sd(rating))
-max(rating_count_pro_user$n)
-min(rating_count_pro_user$n)
+rating_summary_pro_user <- edx %>% group_by(userId) %>% summarise(n = n(), avgRating=mean(rating), sdRating=sd(rating))
+max(rating_summary_pro_user$n)
+min(rating_summary_pro_user$n)
 
-rating_count_pro_user %>% ggplot(aes(n)) + geom_histogram(color="black", binwidth = 5)
-rating_count_pro_user %>% filter(n < 500) %>% ggplot(aes(n)) + geom_histogram(color="black", binwidth = 5)
-rating_count_pro_user %>% filter(n > 500) %>% ggplot(aes(n)) + geom_histogram(color="black", binwidth = 5)
-rating_count_pro_user %>% ggplot(aes(sdRating)) + geom_histogram(color="black", binwidth = 0.1)
+rating_summary_pro_user %>% ggplot(aes(n)) + geom_histogram(color="black", binwidth = 5)
+rating_summary_pro_user %>% filter(n < 500) %>% ggplot(aes(n)) + geom_histogram(color="black", binwidth = 5)
+rating_summary_pro_user %>% filter(n > 500) %>% ggplot(aes(n)) + geom_histogram(color="black", binwidth = 5)
+rating_summary_pro_user %>% ggplot(aes(sdRating)) + geom_histogram(color="black", binwidth = 0.1)
+
+nrow(rating_summary_pro_user[rating_summary_pro_user$sdRating < 0.1,])
+
+sum(rating_summary_pro_user$sdRating == 0)
 
 
 #calculate average ratings pro user. We will use it later
